d7555dd9410406cf777b1070e8fadf24e9f1d0a1
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2018-11-11 20:44:54 +0100

    Outsource constants into dedicated header file

--- model.c
+++ model.c
@@ -2,18 +2,7 @@
 #include <math.h>
 #include <stdlib.h>
 
-#define R_a 287
-#define c_p 1004.  /* unit: hPa */
-#define R_star 8.3144598  /* universal gas constant; unit: J/mol/K */
-#define g 9.80665  /* gravitational acceleration; unit: m/s**2 */
-#define c 299792458  /* speed of light; unit: m/s */
-#define T_universe 2.73  /* temperature of the universe; unit: K */
-#define h 6.62607004E-34  /* unit: m**2 * kg/s */
-#define k_B 1.38064852E-23  /* unit: m**2 * kg / (s**2 * K) */
-#define sigma_SB 5.670367E-8 /* Stefan-Boltzmann constant; unit: W m**-2 * K**-4 */
-#define pi 3.14159265358979323846  /* unit: unitless*/
-#define E_abs 235  /* unit: W/m**2 */
-#define M_air 0.0289644  /* molar mass of Earth's air; unit: kg/mol */
+#include "constants.h"
 
 int negCompare(const void *a, const void *b) {
 	if ((*(double*)b - *(double*)a) < 0)
